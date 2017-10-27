bae6f98a1d9cde6295459486132566ecc26499b4
Riccardo Capraro <capraroriccardo@gmail.com>
2017-10-27 15:23:57 +0200

    Minor bug fix
    A code migration broke an import statement

--- src/lodeg_website/home/lodegML/system.py
+++ src/lodeg_website/home/lodegML/system.py
@@ -126,7 +126,7 @@ class LodegSystem:
         # Get appropriate cache (or set default one if available)
         requested_cache = self._config['cache']
         if requested_cache is not None:
-            from ..lodegML from ..lodegML import cache  # migrate
+            from ..lodegML import cache  # migrate
             # self.check_available_caches()
             # if requested_cache in available_caches:
             # pass # get the appropriate cache somehow
