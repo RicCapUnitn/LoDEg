7e4e21d78b730705a9910937c3789a83bd03dd88
Riccardo Capraro <capraroriccardo@gmail.com>
2018-03-13 09:01:41 +0100

    Fix regression in to_web migration script (fix #53)

--- tools/check_migration.sh
+++ tools/check_migration.sh
@@ -1,7 +1,6 @@
 # Migrate a temp library
 cp -rf src/lodegML/* tools/tmp/migration_backups
 find tools/tmp/migration_backups -type f -name "*.py" -print0 | xargs -0 sed -i "s/from \(configure\.configurations\) import\(.*migrate\)/from ...lodegML\.\1 import\2/g"
-find tools/tmp/migration_backups -type f -name "*.py" -print0 | xargs -0 sed -i "s/from \([\.]\+\)\(.*\) import\(.*migrate\)/from \1lodegML\.\2 import\3/g"
 find tools/tmp/migration_backups -type f -name "*.py" -print0 | xargs -0 sed -i "s/from \([^\.].*\) import\(.*migrate\)/from ..lodegML\.\1 import\2/g"
 find tools/tmp/migration_backups -type f -name "*.py" -print0 | xargs -0 sed -i "s/^import\(.*migrate\)/from ..lodegML import\1/g"
 
@@ -9,7 +8,7 @@ find tools/tmp/migration_backups -type f -name "*.py" -print0 | xargs -0 sed -i 
 find tools/tmp/migration_backups -maxdepth 2 -type f -name "*.py" -exec md5sum {} \; |cut -c-32 |sort > tools/tmp/webmd5.txt
 
 # Compute md5 for the web temporary migrated dir
-find tools/tmp/migration_backups -maxdepth 2 -type f -name "*.py" -exec md5sum {} \; |cut -c-32 |sort > tools/tmp/libmd5.txt
+find src/lodeg_website/home/lodegML -maxdepth 2 -type f -name "*.py" -exec md5sum {} \; |cut -c-32 |sort > tools/tmp/libmd5.txt
 
 # Check differences
 diff -s tools/tmp/libmd5.txt tools/tmp/webmd5.txt
--- tools/lib_to_web.sh
+++ tools/lib_to_web.sh
@@ -8,6 +8,5 @@ cp -r -f src/lodegML/* src/lodeg_website/home/lodegML/
 
 # Run the migration
 find src/lodeg_website/home/lodegML -type f -name "*.py" -print0 | xargs -0 sed -i "s/from \(configure\.configurations\) import\(.*migrate\)/from ...lodegML\.\1 import\2/g"
-find src/lodeg_website/home/lodegML -type f -name "*.py" -print0 | xargs -0 sed -i "s/from \([\.]\+\)\(.*\) import\(.*migrate\)/from \1lodegML\.\2 import\3/g"
 find src/lodeg_website/home/lodegML -type f -name "*.py" -print0 | xargs -0 sed -i "s/from \([^\.].*\) import\(.*migrate\)/from ..lodegML\.\1 import\2/g"
 find src/lodeg_website/home/lodegML -type f -name "*.py" -print0 | xargs -0 sed -i "s/^import\(.*migrate\)/from ..lodegML import\1/g"
